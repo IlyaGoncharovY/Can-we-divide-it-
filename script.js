046b9529c39381781399fd6ea4ed832659a89b1f
Ilya <goncharov.i@rambler.ru>
2021-12-18 12:40:43 +0300

    Условия задачи

---
+++ script.js
@@ -0,0 +1,5 @@
+function isDivideBy(number, a, b) {
+//Your task is to create the functionisDivideBy (or is_divide_by) to 
+//check if an integer number is divisible by both integers a and b.
+   }
+   
